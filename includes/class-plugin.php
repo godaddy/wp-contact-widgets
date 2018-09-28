27e3ae95365e326c826aeb322fe7fa5f0e6a37aa
Evan Herman <evan.m.herman@gmail.com>
2018-09-28 19:13:58 -0400

    Remove include

--- includes/class-plugin.php
+++ includes/class-plugin.php
@@ -24,12 +24,6 @@ class Plugin {
 
 		add_action( 'widgets_init', [ get_called_class(), 'register_widgets' ] );
 
-		if ( ! function_exists( 'is_plugin_active' ) ) {
-
-			require_once ABSPATH . '/wp-admin/includes/plugin.php';
-
-		}
-
 		new Content_Blocks();
 
 	}
